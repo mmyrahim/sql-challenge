9e844aadca08b9b80426999e1dda7380ee1e0a6d
mmyrahim <mmyrahim@outlook.com>
2023-09-20 15:42:28 +0100

    code

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,31 @@
+--List the employee number, last name, first name, sex, and salary of each employee.
+SELECT 
+    e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
+
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT 
+    first_name, last_name, hire_date
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT 
+    d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM dept_manager dm
+JOIN departments d ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT 
+    de.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp de
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+--List each employee in the Sales department, including their employee number, last name, and first name.
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
