1b9cb10df54a6fec037a589965578caf9a59a7a5
mmyrahim <mmyrahim@outlook.com>
2023-09-20 15:35:23 +0100

    data

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -16,16 +16,6 @@ CREATE TABLE employees(
 	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
 
-CREATE TABLE employees(
-	emp_no INT PRIMARY KEY, 
-	emp_title_id VARCHAR(5) NOT NULL, 
-	birth_date DATE NOT NULL,
-	first_name VARCHAR(30) NOT NULL, 
-	last_name VARCHAR(30) NOT NULL,
-	sex CHAR(1) NOT NULL, 
-	hire_date DATE NOT NULL, 
-	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
-);
 
 CREATE TABLE departments(
 	dept_no VARCHAR(5) PRIMARY KEY,
@@ -46,4 +36,10 @@ CREATE TABLE dept_manager(
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
 	PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
